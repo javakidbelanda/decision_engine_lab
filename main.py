4bf1f858239c6aa3f446dcd160a2bc147908a594
javakidbelanda <belandaxjawa@outlook.com>
2026-02-16 21:48:24 +0100

    Day 2: Control & break logic

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
-# Monthly savings calculator
+# Day 2 - Control & Logic
 
-monthly_income = 3500
-monthly_expenses = 2200
-
-monthly_savings = monthly_income - monthly_expenses
-
-print("Monthly Income:", monthly_income)
-print("Monthly Expenses:", monthly_expenses)
-print("Monthly Savings:", monthly_savings)
-annual_interest_rate = 0.05  # 5%
-years = 5
-months = years * 12
+monthly_savings = 1300
+annual_interest_rate = 0.05
 monthly_rate = annual_interest_rate / 12
 
-total_savings = 0
+months = 300
+month = 1
+total = 0
 
-for month in range(1, months + 1):
-    total_savings = (total_savings + monthly_savings) * (1 + monthly_rate)
+while month <= months:
+    total = (total + monthly_savings) * (1 + monthly_rate)
+
+    if total >= 100000:
+        print("You have reached your goal in", month, "months!")
+        break
 
     if month % 12 == 0:
-        print("Year", month // 12, "Total Savings:", round(total_savings, 2))
+        print("Year", [month // 12], "Total", round(total, 2))
+
+    month = month + 1
+
+   
+
+
+
